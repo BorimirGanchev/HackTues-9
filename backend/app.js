e1f56e57c34df0564513eed5194ff0e64edc6a81
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2023-03-09 11:53:25 +0200

    added user system
    linked database to API

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,12 @@ require("dotenv").config();
 //midleware
 
 app.use(express.json());
-app.use("/api/v1/user", router);
+app.use("/user", router);
 const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connection_to_db(
-      /*process.env.MONGO_URI*/ "mongodb+srv://RADU:MONGOPASSWORD@task-manager.8d8g6sk.mongodb.net/HACKTUES?retryWrites=true&w=majority"
+      /*process.env.MONGO_URI*/ "mongodb+srv://HACK:MONGO_HACK@task-manager.8d8g6sk.mongodb.net/HACKTUES?retryWrites=true&w=majority"
     );
     app.listen(port, () => {
       console.log("server is listening on port " + port);
