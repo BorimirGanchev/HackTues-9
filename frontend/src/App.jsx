5f3acaf20df247aa429398fed946a96ea1092c1f
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2023-03-09 14:58:37 +0200

    added map component and react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import {
+  Route,
+  Routes,
+  createBrowserRouter,
+  RouterProvider,
+  BrowserRouter,
+} from "react-router-dom";
 import './App.css'
+import Map from './map'
 
 export default function App(){
   return(
-    <div className="text-3xl font-bold underline">hi</div>
-  )
+    <BrowserRouter>
+   <Routes>
+    <Route path = '/map' element = {<Map />}/>
+   </Routes>
+   </BrowserRouter>
+   )
 }
