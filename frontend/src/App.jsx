782ca096c0227dcedb6ccb992406dd11062b47ee
Ilinats <107075825+Ilinats@users.noreply.github.com>
2023-03-09 13:06:07 +0200

    img slider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import './searchBar'
 import './App.css'
 import SearchBar from './searchBar'
 import Header from './header'
+import Example from './imageSlider'
 
 export default function App(){
   return (
@@ -12,6 +13,7 @@ export default function App(){
       <div className='search-bar'>
         <SearchBar/>
       </div>
+      <Example />
     </div>
   )
 }
