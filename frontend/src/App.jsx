7219b420b33bf9965f0dc74b260bddb0bc1b3cfd
Ilinats <107075825+Ilinats@users.noreply.github.com>
2023-03-09 12:31:00 +0200

    added header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,15 @@ import reactLogo from './assets/react.svg'
 import './searchBar'
 import './App.css'
 import SearchBar from './searchBar'
+import Header from './header'
 
 export default function App(){
   return (
-    <div className='search-bar'>
-      <SearchBar/>
+    <div>
+      <Header/>
+      <div className='search-bar'>
+        <SearchBar/>
+      </div>
     </div>
   )
 }
---
+++ frontend/src/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+export default function Header() {
+
+    return (
+        <div className='header-container'>
+            <h1 className='page-name'>To be changed</h1>
+        </div>
+    )
+}
