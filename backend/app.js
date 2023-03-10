3aa47f2026beff43da08c859b0f7e7cd9e70a608
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2023-03-11 01:50:01 +0200

    added passport and google auth backend. NEEEEEEEEEEEEED SOMEONE TO DO THE FRONTEND

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ app.use(
     origin: ["http://localhost:5173"],
   })
 );
+app.use(express.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // Allow all domains to access the server
@@ -36,6 +37,50 @@ app.use(
 );
 app.use(express.json());
 app.use("/users", users_router);
+require("./config/passport");
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", { successRedirect: "protected" })
+);
+
+app.post("/register", (req, res) => {
+  let user = new UserModel({
+    username: req.body.username,
+    password: hashSync(req.body.password, 10),
+  });
+
+  user.save().then((user) => console.log(user));
+
+  res.send({ success: true });
+});
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
+
+app.get("/protected", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.send("Protected");
+  } else {
+    res.status(401).send({ msg: "Unauthorized" });
+  }
+  console.log(req.session);
+  console.log(req.user);
+});
+
 const port = process.env.PORT || 3000;
 const start = async () => {
   try {
