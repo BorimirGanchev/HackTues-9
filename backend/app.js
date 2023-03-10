7f52654bc01bea141ae4c497cf47a092d0d8014c
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2023-03-10 23:46:33 +0200

    added basic google auth middleware

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,16 @@ const users_router = require("./router/main");
 const connection_to_db = require("./db/main");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const passportMiddleware = require("./passport");
 require("dotenv").config();
 const cors = require("cors");
 app.use(
   cors({
     origin: ["http://localhost:5173"],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
   })
 );
 
@@ -34,6 +39,15 @@ app.use(
     },
   })
 );
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["cyber"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use("/users", users_router);
 const port = process.env.PORT || 3000;
